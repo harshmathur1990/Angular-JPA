4646ac1bf5adfe8a2d41a9ea81b1cc814f49d9f9
harshmathur1990 <harshmathur.1990@gmail.com>
2014-07-04 19:34:29 +0530

    second commit

--- app.js
+++ app.js
@@ -4,18 +4,28 @@
 	app.controller('StoreController', function() {
 		this.products = gems;
 	});
-
+	
+	app.controller('ProductController', function() {
+		this.i = 0;
+		this.setNext = function() {
+			this.i++;
+		};
+		this.setPrev = function() {
+			this.i--;
+		};
+	});
+		
 	var gems = [
 		{
 		 name: "Dodecahedron",
-		 price: 2.95,
+		 price: 2.00,
 		 description: ". . .",
 		 canPurchase: true,
 		 soldOut: false,
 		},
 		{
 		 name: "Pentaginal Gem",
-		 price: 5.95,
+		 price: 8.67,
 		 description: ". . .",
 		 canPurchase: false,
 		 soldOut: false,
