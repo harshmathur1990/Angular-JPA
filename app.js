48fefb28c2bd14398901e1161163c6617d6f72d6
harshmathur1990 <harshmathur.1990@gmail.com>
2014-07-04 20:19:36 +0530

    third ok

--- app.js
+++ app.js
@@ -14,6 +14,18 @@
 			this.i--;
 		};
 	});
+	
+	app.controller('PanelController', function() {
+		this.tab = 1;
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+		
+	});
 		
 	var gems = [
 		{
