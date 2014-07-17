e6ea0ca528fc71c38d5a956109906327891b8d25
harshmathur1990 <harshmathur.1990@gmail.com>
2014-07-17 09:22:50 +0530

    CRUD

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,17 +1,34 @@
 package com.tutorialspoint;
 
+import java.util.List;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.tutorialspoint.StudentJDBCTemplate;
 
 public class MainApp {
 	public static void main(String[] args) {
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"Beans.xml");
-		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-		TestHello testobj = (TestHello) context.getBean("testhello");
-		obj.getMessage();
-		testobj.getIamint();
-		context.registerShutdownHook();
+		StudentJDBCTemplate studentJDBCTemplate = (StudentJDBCTemplate) context
+				.getBean("studentJDBCTemplate");
+		studentJDBCTemplate.create_table_if_not_exists();
+		System.out.println("------Records Creation--------");
+		studentJDBCTemplate.create("Zara", 11);
+		studentJDBCTemplate.create("Nuha", 2);
+		studentJDBCTemplate.create("Ayan", 15);
+		System.out.println("------Listing Multiple Records--------");
+		List<Student> students = studentJDBCTemplate.listStudents();
+		for (Student record : students) {
+			System.out.print("ID : " + record.getId());
+			System.out.print(", Name : " + record.getName());
+			System.out.println(", Age : " + record.getAge());
+		}
+		System.out.println("----Updating Record with ID = 2 -----");
+		studentJDBCTemplate.update(2, 20);
+		System.out.println("----Listing Record with ID = 2 -----");
+		Student student = studentJDBCTemplate.getStudent(2);
+		System.out.print("ID : " + student.getId());
+		System.out.print(", Name : " + student.getName());
+		System.out.println(", Age : " + student.getAge());
 	}
 }
